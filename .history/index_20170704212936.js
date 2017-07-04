860acb83495fabe80c7dbe0675c52db3792a2bea
unknown <jangdk@ebaykorea.corp>
2017-07-04 21:30:38 +0900

    수정
    수정

--- .history/index_20170704212936.js
+++ .history/index_20170704212936.js
@@ -14,6 +14,6 @@ app.get('/' , function(req , res){
 // });
 
 app.listen(process.env.PORT || 8080, function(){
-    console.log("Express server listening on port %d in %s mode");
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
